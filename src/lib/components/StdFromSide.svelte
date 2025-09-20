c63066b5e60dbae04a6511aaf33a965736de26e7
darkicewolf50 <darkicewolf50@gmail.com>
2025-09-20 11:59:13 -0600

    feat(team)^: restarted team page to use less sytling selectors

---
+++ src/lib/components/StdFromSide.svelte
@@ -0,0 +1,22 @@
+<script>
+	let { children, backgroundColour, columnGap } = $props();
+	$inspect(backgroundColour);
+</script>
+
+<div style:background-color={`var(${backgroundColour})`} style:column-gap={columnGap}>
+	<div>
+		{@render children?.()}
+	</div>
+</div>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		padding: 1svb 0svw;
+	}
+	div > div {
+		max-width: 80svw;
+	}
+</style>
