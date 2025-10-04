9f85070ce9b68ed30e6fd92dc9905b2de6e3278b
ahmad.ahmad1 <ahmad.ahmad1@ucalgary.ca>
2025-10-04 11:02:52 -0600

    feat(Sponsors): Added static sponsors, updated sponsor logos.

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -9,4 +9,502 @@
 	imgUrl="https://res.cloudinary.com/dpgrgsh7g/image/upload/v1755914692/IMG_6671_daaeeq.jpg"
 />
 
-<h1>Hello</h1>
+<div id="OurSponsors">
+  <div id="SponsorsAbout">
+    <h2>Our partners are the foundation of our success.</h2>
+    <p>
+      By supporting us, you're helping shape the next generation of engineers.
+      Our club members develop real-world skills, problem-solving abilities, and
+      teamwork. And of course, with your help, we're one step closer to victory.
+    </p>
+    <ul>
+      <li><a href="/">Learn More</a></li>
+      <li><a href="/">Support Us</a></li>
+    </ul>
+  </div>
+
+  <div id="Sponsor">
+    <div class="Sponsors">
+      <h3>Diamond Tier</h3>
+      <div>
+        <a
+          href="https://schulich.ucalgary.ca/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img
+              alt="Schulich School of Engineering's Logo"
+              src="https://static.wixstatic.com/media/5824fc_5fbf9755b7a5425390262c6f5ac17044~mv2.png/v1/fill/w_426,h_204,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/5824fc_5fbf9755b7a5425390262c6f5ac17044~mv2.png"
+            />
+            <div>
+              <h4>Schulich School of Engineering</h4>
+              <p>
+                The Schulich School of Engineering at the University of Calgary
+                is the foundation of Baja at UCalgary. Schulich allows the team
+                to be able to turn their dreams of creating an off-roading car
+                into a reality.
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="Sponsors">
+      <h3>Platinum Tier</h3>
+      <div>
+        <a href="https://darkicewolf50.dev" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="darkicewolf50's Logo"
+              src="https://darkicewolf50.dev/favicon.png"
+            />
+            <div>
+              <h4>darkicewolf50</h4>
+              <p></p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="Sponsors">
+      <h3>Gold Tier</h3>
+      <div>
+        <a
+          href="https://www.surimanufacturing.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img
+              alt="Suri's Logo"
+              src="https://lh3.googleusercontent.com/sitesv/AICyYdbOtAQlGZ6ycRObSizXmwokTeD5HCJTiDfMvhNJk65ZXPmcgvBBwUzUUU55XXZcdfNKU-kN-MglEccrqBkMy1DoxkbBJpBiVsyeLq7nEkPDS2GY8W0UgRzfbJJtR-YP322JEe9D3_jWqI1WKDQHZmL0DDbQsAfQiDHuYBeHHkYQdenXWJtRZJ19dMZjuL2BQspzXQjoz0IvQK7mwr5OF8q3HMIil7EVD7IX=w1280"
+            />
+            <div>
+              <h4>Suri</h4>
+              <p></p>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://www.skf.com/ca/en" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="SKF's Logo"
+              src="https://www.skf.com/v2/assets/img/skf-logo-white.svg"
+            />
+            <div>
+              <h4>SKF</h4>
+              <p></p>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://wildrosemx.com/" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="WRMA's Logo"
+              src="https://i0.wp.com/wildrosemx.com/wp-content/uploads/2024/07/SITE-HEADER-2.png?fit=400%2C200&ssl=1"
+            />
+            <div>
+              <h4>WRMA</h4>
+              <p></p>
+            </div>
+          </div>
+        </a>
+
+        <a
+          href="https://www.troteclaser.com/en-ca/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img
+              alt="Trotec's Logo"
+              src="https://www.troteclaser.com/_nuxt/img/logo-trotec.709e7ce.svg"
+            />
+            <div>
+              <h4>Trotec</h4>
+              <p></p>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://www.solidworks.com/" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="Solidworks's Logo"
+              src="https://www.3ds.com/assets/3ds-navigation/3DS_corporate-logo_solidworks.svg"
+            />
+            <div>
+              <h4>Solidworks</h4>
+              <p></p>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://www.kisssoft.com/en" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="KISSsoft's Logo"
+              src="https://www.kisssoft.com/images/kisssoft-logo.svg"
+            />
+            <div>
+              <h4>KISSsoft</h4>
+              <p></p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="Sponsors">
+      <h3>Silver Tier</h3>
+      <div>
+        <a
+          href="https://www.encoremetals.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img
+              alt="Encore Metals's Logo"
+              src="https://www.encoremetals.com/wp-content/uploads/2023/12/brand-logo-web-dark.webp"
+            />
+            <div class="sponsorCenter">
+              <h4>Encore Metals</h4>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://cnoocinternational.com/" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="CNOOC's Logo"
+              src="https://cnoocinternational.com/wp-content/uploads/2025/01/cnooc-logo-2025-e1735841513450-768x212.png"
+            />
+            <div class="sponsorCenter">
+              <h4>CNOOC</h4>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://www.polaris.com/en-ca/" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="Polaris's Logo"
+              src="https://cdn1.polaris.com/globalassets/crp/2024/nav-bar-logos/nav-bar-logo-polaris.svg?v=c0e00521"
+            />
+            <div class="sponsorCenter">
+              <h4>Polaris</h4>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="Sponsors">
+      <h3>Bronze Tier</h3>
+      <div>
+            <a href="https://www.canadaaction.ca/" target="_blank" rel="noreferrer">
+          <div>
+            <img
+              alt="Canada Action's Logo"
+              src="https://assets.nationbuilder.com/canadaaction/sites/1044/meta_images/original/logo_ca_tall_transparent_white.png?1713373970"
+            />
+            <div class="sponsorCenter">
+              <h4>Canada Action</h4>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div id="SponsorAfter">
+    <h2>Sponsorship Information</h2>
+    <p>
+      Our sponsors are the backbone of UCalgary Baja's success, providing support
+      that allows our members to gain hands-on engineering experience as part of
+      their education. Our team is entirely student-run, allowing members to
+      develop skills beyond engineering, including design, fundraising, sourcing
+      materials, team management, and project coordination.
+    </p>
+    <p>
+      We deeply value the partnership with our sponsors, as their support makes
+      every project and competition possible.
+    </p>
+    <p>
+      If you're interested in sponsoring UCalgary Baja, please contact our team
+      captain at
+      <a href="mailto:uofcbaja@gmail.com"><span>uofcbaja@gmail.com</span></a>.
+    </p>
+    <p>
+      Below, you can find our sponsorship package detailing the opportunities
+      available.
+    </p>
+    <div>
+      <a
+        href="https://drive.google.com/file/d/132qCyCNnqtVGPQiedJHb8BNBo-mz8apl/view?usp=sharing"
+      >
+        <button>Sponsorship Package</button>
+      </a>
+    </div>
+  </div>
+</div>
+
+<style>
+	:root {
+	--tableBackground: white;
+	--sponsorBorderWidth: 2px;
+	--sponsorBorderStyle: solid;
+	--sponsorBorderColour: black;
+	--sponsorBorder: var(--sponsorBorderWidth) var(--sponsorBorderStyle)
+		var(--sponsorBorderColour);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+
+#OurSponsors img {
+	object-fit: contain;
+}
+
+#SponsorsAbout {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 4svh 0svw;
+	background-color: var(--BajaBlack);
+	color: white;
+}
+
+#SponsorsAbout h2 {
+	margin: 0px;
+	padding: 2svh 0svw;
+	padding-top: 4svh;
+	font-size: 30px;
+}
+
+#SponsorsAbout p {
+	font-size: larger;
+	padding: 0px;
+	margin: 0px;
+	max-width: 120ch;
+	text-align: center;
+}
+
+#SponsorsAbout ul {
+	list-style-type: none;
+	display: flex;
+	flex-flow: row wrap;
+	column-gap: 4svw;
+	padding: 0svh 0svw;
+	padding-top: 4svh;
+}
+
+#SponsorsAbout a {
+	text-decoration: none;
+	color: white;
+	background-color: var(--BajaRed);
+	border-radius: 15px;
+	padding: 2svh 2svw;
+	font-size: large;
+}
+
+#SponsorsAbout a:hover {
+	background-color: var(--BajaHover);
+}
+
+#Sponsor {
+	display: flex;
+	flex-direction: column;
+	padding-bottom: 3svh;
+	background-color: var(--BajaBlack);
+	color: white;
+}
+
+#Sponsor .Sponsors h3 {
+	font-size: xx-large;
+	margin: 4svh 1svw;
+}
+
+#Sponsor .Sponsors h4 {
+	font-size: xx-large;
+	margin: 1svh 1svw;
+	text-align: center;
+}
+
+#SponsorEnd {
+	text-align: end;
+}
+
+#OurSponsors h1 {
+	position: relative;
+	text-align: left;
+	border-bottom: var(--sponsorBorder);
+	border-top: var(--sponsorBorder);
+	width: fit-content;
+	padding-top: 2svh;
+	padding-bottom: 2svh;
+}
+
+#OurSponsors #h1Exception {
+	border-top: none;
+}
+
+#Sponsor .Sponsors h1 {
+	width: 100%;
+	border: none;
+}
+
+.Sponsors {
+	display: flex;
+	flex-direction: column;
+	background-color: inherit;
+	align-items: center;
+	/* width: 70svw; */
+}
+
+.Sponsors div {
+	display: flex;
+	flex-flow: row wrap;
+	padding: 2svh 2svw;
+	column-gap: 2svw;
+	row-gap: 2svh;
+	justify-content: center;
+}
+
+.Sponsors a {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	color: inherit;
+	text-decoration: none;
+	cursor: pointer;
+	background-color: rgba(38, 38, 38, 0.5);
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	transition: scale 0.3s;
+	padding: 1svh 1svw;
+	border-radius: 1rem;
+}
+
+.Sponsors a:hover {
+	cursor: pointer;
+	scale: 1.05;
+}
+
+.Sponsors a div {
+	display: flex;
+	flex-flow: row nowrap;
+	height: inherit;
+	padding: 0px;
+	column-gap: 1svw;
+	align-items: center;
+}
+
+@media only screen and (max-width: 800px) {
+	.Sponsors a div {
+		flex-wrap: wrap;
+	}
+}
+
+.Sponsors a div div {
+	flex-direction: column;
+	row-gap: 0.5svh;
+	min-width: 30ch;
+	max-width: 60ch;
+	margin: 2svh 0svw;
+}
+
+.Sponsors a div div p {
+	margin: 0px;
+}
+
+.Sponsors a h3 {
+	display: flex;
+	/* width: fit-content; */
+	border-bottom: var(--sponsorBorder);
+}
+
+.Sponsors a img {
+	min-height: 10svh;
+	max-height: 24svh;
+	min-width: 240px;
+	max-width: 240px;
+	padding: 1svh 0.5svw;
+	background-color: grey;
+	border-radius: 0.5rem;
+}
+
+.sponsorCenter {
+	width: 72%;
+}
+
+@media only screen and (max-width: 1025px) {
+	.Sponsors a {
+		flex-grow: 1;
+	}
+
+	.Sponsors a div div {
+		max-width: none;
+		min-width: 1px;
+	}
+}
+
+#SponsorAfter {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 4svh 0svw;
+	background-color: var(--BajaBlack);
+	color: white;
+	text-align: center;
+}
+
+#SponsorAfter p {
+	max-width: 120ch;
+	font-size: large;
+	/* margin: 0px; */
+}
+
+#SponsorAfter h2 {
+	max-width: 120ch;
+	margin: 0px;
+	padding: 2svh 0svw;
+	padding-top: 4svh;
+	font-size: 30px;
+}
+
+#SponsorAfter a {
+	text-decoration: none;
+	color: inherit;
+	display: inline-block;
+}
+
+#SponsorAfter a:hover {
+	color: var(--BajaRed);
+}
+
+#SponsorAfter a h4 {
+	display: inline-block;
+	margin: 0px;
+}
+
+#SponsorAfter button {
+	text-decoration: none;
+	color: white;
+	background-color: var(--BajaRed);
+	border-radius: 15px;
+	padding: 2svh 4svw;
+	font-size: large;
+	border: none;
+}
+
+#SponsorAfter button:hover {
+	background-color: var(--BajaHover);
+}
+
+</style>
