47065ae45733445b639ada3523185a811f0fdfa2
ahmad.ahmad1 <ahmad.ahmad1@ucalgary.ca>
2025-10-18 13:46:13 -0600

    feat(Merch): Added item tiles, item page, merch top banner, merch header.

--- src/routes/merch/+layout.svelte
+++ src/routes/merch/+layout.svelte
@@ -1,6 +1,94 @@
 <script>
 	import '$lib/assets/global.css';
+	import TopBanner from '$lib/components/TopBanner.svelte';
 	let { children } = $props();
 </script>
-<!-- Merch header -->
+
+<header>
+<div></div>
+<nav>
+	<ul>
+		<li>
+			<a href={"/merch"}>ALL DROPS</a>
+		</li>
+		<li>
+			<a href={"/merch/heavy"}>HEAVY WEAR</a>
+		</li>
+		<li>
+			<a href={"/merch/soft"}>SOFT WEAR</a>
+		</li>
+		<li>
+			<a href={"/merch/accesories"}>ACCESORIES</a>
+		</li>
+	</ul>
+</nav>
+<a href="/merch/checkout">
+	<img
+		src={"https://www.svgrepo.com/show/470579/cart.svg"}
+		alt="Cart Logo"
+	/></a>
+</header>
+
+
+<style>
+
+	
+header {
+	display: flex;
+	height: 84px;
+	padding-right: 0.5svw;
+	background-color: var(--BajaBlack);
+}
+a {
+	cursor: pointer;
+	flex: 1;
+	height: inherit;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+div {
+	flex: 1;
+}
+img {
+	aspect-ratio: 1;
+	height: 8svh;
+	padding: 0svh 0.15rem;
+	margin-left: 3svw;
+	filter: invert();
+
+}
+nav {
+	flex-grow: 1;
+	height: inherit;
+}
+ul {
+	flex-flow: row wrap;
+	justify-content: center;
+	column-gap: 2svw;
+	height: inherit;
+	align-items: center;
+    display: flex;
+	margin: 0px;
+	padding: 0px;
+	list-style: none;
+}
+nav a {
+	text-decoration: none;
+	cursor: pointer;
+	background-color: var(--BajaBlack);
+	color: whitesmoke;
+	font-size: larger;
+}
+nav a:hover {
+	color: var(--BajaRed);
+}
+
+@media only screen and (max-width: 800px) {
+	nav ul {
+		justify-content: space-evenly;
+	}
+}
+</style>
+
 {@render children?.()}
--- src/routes/merch/heavy/+page.svelte
+++ src/routes/merch/heavy/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import TopBanner from '../MerchTopBanner.svelte';
+	import Item from '../item.svelte';
 </script>
 <TopBanner
-	titleText="Checkout"
+	titleText="Heavy Wear"
 	imgUrl="https://res.cloudinary.com/dpgrgsh7g/image/upload/v1755914996/DSC_0393_inuw2z.jpg"
 />
+<Item/>
