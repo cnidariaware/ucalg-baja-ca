4a0971e3ca754b48198680dc18e79f2bcffc1b5d
Siddharth <siddharthnarendrakum@ucalgary.ca>
2025-11-08 16:02:46 -0700

    Feat(Sponsor)^: Displaying images properly (#2)

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -1,24 +1,15 @@
 <script>
 	import TopBanner from '$lib/components/TopBanner.svelte';
 	let sponsorInfo = $state('');
+	let order = ['Diamond Tier', 'Platinum Tier', 'Gold Tier', 'Silver Tier', 'Bronze Tier'];
 	let gotThing = $state(false);
 	const fetchingData = async () => {
 		let sponsors = await fetch('http://localhost:6526/sponsors');
-		// sponsorInfo = await sponsors.json();
 		let test = await sponsors.json();
-
-		// Iterate through each tier
-		// for (let tier in test) {
-		// 	console.log('Tier:', tier);
-
-		// 	// Iterate through each sponsor in that tier
-		// 	for (let i = 0; i < test[tier].length; i++) {
-		// 		console.log(test[tier][i].SponsorName);
-		// 	}
-		// }
 
 		sponsorInfo = await test;
 		gotThing = true;
+		$inspect(sponsorInfo);
 	};
 
 	$inspect(sponsorInfo);
@@ -35,22 +26,31 @@
 <button onclick={fetchingData}> Press me </button>
 {#if sponsorInfo !== ''}
 	<div>
-		{#each Object.keys(sponsorInfo) as tier, index}
-			<h2>{tier}</h2>
-			{#each sponsorInfo[tier] as item, i}
-				<img alt="Blah" src={item.LogoUrl} />
+		{#each order as tier}
+			{#if sponsorInfo[tier]}
+				<h2>{tier}</h2>
 				<div>
-					<h4>
-						{item.DescriptionAboutSponsor}
-					</h4>
+					{#each sponsorInfo[tier] as item}
+						<div>
+							<img alt="Blah" src={item.LogoUrl} width="35px" />
+							<div>
+								<h4>
+									{item.SponsorName}
+								</h4>
+								{#if tier == order[0] || tier == order[1]}
+									<h5>{item.DescriptionAboutSponsor}</h5>
+								{/if}
+							</div>
+						</div>
+					{/each}
 				</div>
-			{/each}
+			{/if}
 		{/each}
 	</div>
 {/if}
 
 <h4>Sponsorship Benefits</h4>
-<p1>
+<p>
 	Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
 	pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed
 	diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl
@@ -61,14 +61,16 @@
 	fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc
 	posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia
 	nostra inceptos himenaeos.
-</p1>
+</p>
 
 <style>
+	div {
+		background-color: gray;
+	}
 	h4 {
 		text-align: center;
 		display: block;
 		color: #000;
-		font-family: 'Times New Roman', Times, serif;
 		font-size: 1.5em;
 		margin-top: 1.33em;
 		margin-bottom: 1.33em;
@@ -77,11 +79,46 @@
 	}
 	img {
 		display: flex;
+		justify-content: left;
+		align-items: center;
+		width: 20%;
+		height: 20%;
+		margin: auto;
+		padding: 5px;
+	}
+	div {
+		padding: 10px;
+		border-style: none;
+		border-width: 1px;
+	}
+	h2 {
+		display: flex;
 		justify-content: center;
-		width: 150px;
 	}
 	div > div {
-		text-align: left;
 		display: flex;
+		justify-content: center;
+		column-gap: 50px;
+		flex-wrap: wrap;
+
+		align-items: center;
+		height: 50%;
+		width: 100%;
+	}
+	div > div > div {
+		border-style: solid;
+		min-width: 10svw;
+		max-width: 70%;
+		text-align: right;
+		display: flex;
+		justify-content: left;
+		padding: 10px;
+	}
+	div > div > div > div {
+		border-style: none;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
 	}
 </style>
