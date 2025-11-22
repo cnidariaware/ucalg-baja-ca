1df5a6b0010c9e4b451e4d8f6f5240234c6eafa9
Siddharth <siddharthnarendrakum@ucalgary.ca>
2025-11-22 12:08:05 -0700

    Feat(Sponsor): Sponsorship package and finsihed styling(#2)

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -53,11 +53,154 @@
 			{/each}
 		</div>
 	{/if}
-	<img
-		src="https://res.cloudinary.com/dqr21nexg/image/upload/v1763606815/SponsorShip_package_fyb9uv.png"
-		alt="Sponsorship Package"
-	/>
 	<h4>Sponsorship Benefits</h4>
+	<table class="sponsor-table">
+		<thead>
+			<tr>
+				<th></th>
+				<th>Bronze<br />&gt;$250</th>
+				<th>Silver<br />$1,000</th>
+				<th>Gold<br />$2,500</th>
+				<th>Platinum<br />$5,000</th>
+				<th>Diamond<br />$10,000</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- Logo on Website -->
+			<tr>
+				<th>Logo On<br />Website</th>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+			</tr>
+
+			<!-- Social Media Exposure -->
+			<tr>
+				<th>Social Media<br />Exposure</th>
+				<td></td>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+			</tr>
+
+			<!-- Shop Tour / Introduction -->
+			<tr>
+				<th>Shop Tour /<br />Introduction</th>
+				<td></td>
+				<td></td>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+			</tr>
+
+			<!-- Social Media Feature -->
+			<tr>
+				<th>Social Media<br />Feature</th>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+				<td
+					><img
+						class="cross"
+						src="https://www.svgrepo.com/show/491212/cross.svg"
+						alt="Included"
+					/></td
+				>
+			</tr>
+
+			<!-- Logo on Vehicle -->
+			<tr>
+				<th>Logo On<br />Vehicle</th>
+				<td>Small</td>
+				<td>Small</td>
+				<td>Medium</td>
+				<td>Large</td>
+				<td>Full Body<br />Panel</td>
+			</tr>
+		</tbody>
+	</table>
 	<p>
 		Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
 		pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
@@ -71,8 +214,15 @@
 		conubia nostra inceptos himenaeos.
 	</p>
 </div>
+<div>
+	<!-- Contact form -->
+</div>
 
 <style>
+	td > img {
+		margin: auto;
+		filter: invert();
+	}
 	a {
 		text-decoration: none;
 	}
@@ -87,7 +237,7 @@
 		color: var(--BajaWhite);
 		font-size: 3em;
 	}
-	img {
+	a > img {
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: left;
@@ -103,6 +253,7 @@
 		padding: 5px;
 		border-radius: 15px;
 	}
+
 	div > div {
 		padding: 10px;
 		border-style: none;
@@ -140,6 +291,7 @@
 		margin: 10px;
 		border-radius: 15px;
 	}
+
 	div > div > div > div > a > div {
 		border-style: none;
 		text-align: center;
@@ -160,5 +312,42 @@
 		padding-left: 10%;
 		padding-right: 10%;
 		padding-bottom: 25px;
+	}
+	table {
+		margin-top: 75px;
+		border-collapse: collapse;
+		width: 100%;
+		max-width: 900px;
+		color: var(--BajaWhite);
+		border-style: hidden;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 75px;
+	}
+
+	.sponsor-table th,
+	.sponsor-table td {
+		border: 3px solid var(--BajaWhite);
+		border-radius: 100%;
+		padding: 16px;
+		text-align: center;
+		vertical-align: middle;
+		font-size: 18px;
+	}
+
+	.sponsor-table thead th {
+		font-size: 22px;
+		font-weight: 600;
+	}
+
+	.sponsor-table th:first-child {
+		text-align: left;
+		width: 30%;
+	}
+
+	.sponsor-table .cross {
+		width: 40px;
+		height: 40px;
+		object-fit: contain;
 	}
 </style>
