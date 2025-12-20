d9b9bfce0c27337c4c72f4f4e968089e7e030c3d
darkicewolf50 <brock@darkicewolf50.dev>
2025-12-20 14:31:42 -0700

    feat(sponsor): now hover effect and bolding present
    Signed-off-by: darkicewolf50 <brock@darkicewolf50.dev>

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -146,7 +146,7 @@
 		fundraising, sourcing materials, team management, and project coordination. We deeply value the
 		partnership with our sponsors, as their support makes every project and competition possible. If
 		you're interested in sponsoring UCalgary Baja, please contact our team captain at
-		<a href="mailto:club@ucalgarybaja.ca">club@ucalgarybaja.ca</a>. Below, you can find our sponsorship package detailing the opportunities
+		<a href="mailto:club@ucalgarybaja.ca"><strong>club@ucalgarybaja.ca</strong></a>. Below, you can find our sponsorship package detailing the opportunities
 		available.
 	</p>
 	<h4>Sponsor Us</h4>
@@ -174,7 +174,13 @@
 	}
 	a {
 		text-decoration: none;
+		color: inherit;
+
+		&:hover {
+			color: var(--BajaRed)
+		} 
 	}
+
 	div {
 		background-color: var(--BajaBlack);
 		padding-top: 0.33em;
